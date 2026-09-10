584977cc27a201c1d587a3fab2b829106c8b440b
VALENTIN NUÑEZ <valencho912@gmail.com>
2026-09-10 19:44:07 -0300

    Creacion de codigo
    creación y diseño del codigo.

---
+++ tp3ej2/src/tp3ej2/Tp3ej2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3ej2;
+
+/**
+ *
+ * @author Valentin
+ */
+public class Tp3ej2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
